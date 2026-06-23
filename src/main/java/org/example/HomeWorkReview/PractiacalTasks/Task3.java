015a7ecd557efb70d7017b3eaed342d18c60c9c3
Bedchel <grishaev.ma12@gmail.com>
2026-06-23 18:19:35 +0200

    Update3.5

--- src/main/java/org/example/HomeWorkReview/PractiacalTasks/Task3.java
+++ src/main/java/org/example/HomeWorkReview/PractiacalTasks/Task3.java
@@ -4,22 +4,40 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Task3 {
+    static Scanner scanner = new Scanner(System.in);
+
     static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int lastNumber = 0;
-        int summeOfNumbers = 0;
-        while (true)
+        int lastNumber = getLastNumber();
+
+        int summeOfNumbers = calculateSum(lastNumber);
+
+        System.out.println(STR."Сума від 1 до \{lastNumber} це \{summeOfNumbers}");
+    }
+
+    public static int getLastNumber() {
+        while (true) {
             try {
                 System.out.println("До якого числа обчислювати?");
-                lastNumber = scanner.nextInt();
-                break;
+                int number = scanner.nextInt();
+
+                if (number <= 0) {
+                    System.out.println("Помилка! Число має бути більшим за 0.");
+                    continue;
+                }
+
+                return number;
             } catch (InputMismatchException e) {
-                System.err.println("Помилка число незнайдено\n" +
-                        "Повторить спробу");
+                System.err.println("Помилка! Число не знайдено.\nПовторіть спробу");
+                scanner.nextLine();
             }
-        for (int i = 0; i < lastNumber + 1; i++) {
-            summeOfNumbers += i;
         }
-        System.out.println(STR."Сумма від одного до \{lastNumber} це \{summeOfNumbers}");
+    }
+
+    public static int calculateSum(int lastNumber) {
+        int sum = 0;
+        for (int i = 1; i <= lastNumber; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
